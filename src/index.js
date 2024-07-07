fa0bc3f7b973b05b60fb8c191a1b74a0838fab5d
Philip osir Otieno <philiposir2@gmail.com>
2024-07-07 20:45:30 +0300

    added task list to the Documment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let taskForm = document.getElementById("create-task-form");
+  taskForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    let taskDesc = document.getElementById("new-task-description").value;
+    handleToDo(taskDesc);
+    });
 });
+
+function handleToDo(todo) {
+  let li = document.createElement('li')
+  // let btn = document.createElement('button')
+  // btn.addEventListener("click",deletBtn)
+  li.textContent = `${todo}  `
+  // btn.textContent = 'X'
+  // li.appendChild(btn)
+  document.getElementById("tasks").appendChild(li)
+}
+
+function deletBtn(e) {
+  e.target.parentNode.remove()
+}
